522e8ef6fd76adc814737344378aef817c9dfd96
ru14 <74753345+ru14@users.noreply.github.com>
2021-05-04 16:17:18 -0700

    error fixed

--- server.js
+++ server.js
@@ -231,7 +231,7 @@ function runSearch() {
                     removeEmployees();
                     break;
                 case "Update employee role.":
-                    upadateEmployees();
+                    upadateEmployeeRole();
                     break;
                 case "Upadte employee manager.":
                     upadateEmployeesManager();
@@ -295,21 +295,92 @@ function viewEmployeeByRoles() {
 };
 
 function viewEmployeeByManager() {
-    let query = " CONCAT(manager.first_name,manager.last_name) AS manager, employee.first_name, employee.last_name ";
-    query += " FROM MANAGER ";
-    query += " LEFT JOIN employee manager ON manager.id = employee.manager_id ";
-    query += " ORDER BY manager.first_name ";
-    //console.log({query})
-    db.query(query, function (err, res) {
-        if (err) throw err;
-        //console.log({res})
-        console.table('Employees By Manager', res);
-        runSearch()
+    db.query('SELECT employee.manager_id, CONCAT(manager.first_name," " ,manager.last_name) AS manager FROM EMPLOYEE', function (err, response) {
+        if (err) {
+            throw err;
+        }
+        console.log(response);
+    // let query = "SELECT CONCAT(manager.first_name,manager.last_name) AS manager FROM EMPLOYEE, roles.title, department.dept_name AS Department, roles.salary, employee.first_name, employee.last_name ";
+    // query += " FROM EMPLOYEE ";
+    // query += " LEFT JOIN manager ON manager.id = employee.manager_id ";
+    // query += " ORDER BY manager.first_name ";
+    // //console.log({query})
+    // db.query(query, function (err, res) {
+    //     if (err) throw err;
+    //     //console.log({res})
+    //     console.table('Employees By Manager', res);
+    //     runSearch()
     })
 };
 
 
 
 //module.exports = db;
-// function upadateEmployees(){
+// function upadateEmployeeRole(){
+    // function addEmployees() {
+    //     db.query('SELECT CONCAT(employee.first_name," " ,employee.last_name) AS name title,id, salary, department_id FROM ROLES', function (err, response) {
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         console.log(response);
+    //         inquirer.prompt([
+    //             {
+    //                 type: "input",
+    //                 name: "first_name",
+    //                 message: "Enter first name of employee:"
+    //             }, {
+    
+    //                 type: "input",
+    //                 name: "Last_name",
+    //                 message: "Enter last name of employee:"
+    //             }, {
+    //                 type: "list",
+    //                 message: "Add employee role:",
+    //                 name: "role",
+    //                 choices() {
+    //                     const choiceArray = [];
+    //                     response.forEach(({ title }) => {
+    //                         choiceArray.push(title);
+    //                     });
+    //                     //console.log({choiceArray});
+    
+    //                     return choiceArray;
+    //                 }
+    //             },
+    //         ])
+    
+    //             .then((answer) => {
+    
+    //                 let chosenItem;
+    //                 response.forEach((item) => {
+    //                     if (item.title === answer.role) {
+    //                         chosenItem = item;
+    //                     }
+    //                 });
+    
+    //                 //console.log("Choice: " + answer);
+    //                 // console.log("Role: " + answer.role);
+    //                 //response.forEach(item => {
+    //                 //    console.log("Item: " + item.title + " " + item.id);
+    //                 //})
+    
+    //                 //console.log("ChosenItem: " + chosenItem.title + " " + chosenItem.id);
+    
+    //                 const query = `INSERT INTO EMPLOYEE (first_name,Last_name,roles_id) VALUE(?,?,?) `
+    
+    //                 db.query(
+    
+    //                     query, [answer.first_name, answer.Last_name, chosenItem.id], function (err, res) {
+    //                         if (err) throw err;
+    //                         console.table(`${answer.first_name, answer.last_name}added`, res);
+    //                         runSearch()
+    //                     })
+    
+    //             })
+    //     })
+    //     //console.log({query})c
+    
+    // };
+    // let query = " UPDATE EMPLOYEE ";
+    // query += " SET  roles_id = "
 // function endSession()
